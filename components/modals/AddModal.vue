2bb0ffb893fa997e78266ea6acff7dca05524551
gluscevicluka <gluscevicluka@gmail.com>
2025-05-21 19:11:08 +0200

    fix button option

--- components/modals/AddModal.vue
+++ components/modals/AddModal.vue
@@ -103,10 +103,11 @@
           </div>
           <button
             type="button"
-            class="text-green-600 hover:text-green-800 text-sm"
+            class="flex mt-3 w-full items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             @click="addOption"
           >
-            + Add Option
+            <img src="/assets/img/add-button-vector.svg" alt="Icon" /> &nbsp;
+            Add Option
           </button>
           <p v-if="errors.options" class="text-red-500 text-sm mt-1">
             {{ errors.options }}
